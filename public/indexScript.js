6f031a49de8f9a62b746c8216ff80b6cbc1612d7
Will Murray <williammurray29@gmail.com>
2023-01-11 18:39:12 -0800

    Added Homepage Animations

--- public/indexScript.js
+++ public/indexScript.js
@@ -1,6 +1,30 @@
 let Gallery = document.getElementById("GalleryBox")
-let icons = document.querySelectorAll(".icon");
-
+let SandboxBTN = document.getElementById("icon")
 Gallery.onclick = function(){
     window.open("gallery.html")
 }
+Gallery.onmouseover = function(){
+    Gallery.style.backgroundColor = "cornflowerblue"
+    Gallery.style.color = "white"
+    
+}
+Gallery.onmouseout = function(){
+    Gallery.style.backgroundColor = "white"
+    Gallery.style.color = "black"
+    
+}
+
+
+SandboxBTN.onclick = function(){
+    window.open("realtime.html")
+}
+
+SandboxBTN.onmouseover = function(){
+    SandboxBTN.style.backgroundColor = "cornflowerblue"
+    SandboxBTN.style.color = "white"
+    
+}
+SandboxBTN.onmouseout = function(){
+    SandboxBTN.style.backgroundColor = "white"
+    SandboxBTN.style.color = "black"
+}  
